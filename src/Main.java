62c708e09da3500cdf881fcb5f6f33e3f5ae783c
name:"Dev <username:"devmeena7000@gmail.com>
2024-10-03 23:44:31 +0530

    New checkpoint

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,47 @@
 public class Main {
-    public static void main(String[] args) {
-    Pen p1 =new Pen();
-    p1.setColor("orange");
-        System.out.println(p1.getColor());
-        p1.setTip(4);
-        System.out.println(p1.getTip());
-    Pen p2 = new Pen();
-    p2.setColor("green");
-        System.out.println(p2.getColor());
-        p2.setTip(6);
-        System.out.println(p2.getTip());
-    }
-}
-class Pen{
-    private String color;
-    private int tip;
+  public int[] twoSum(int[] nums,int target){
 
-    String getColor(){
-    return this.color;
-    }
-    int getTip(){
-        return this.tip;
-    }
-    void setColor(String newColor){
-    this.color = newColor;
-    }
-    void setTip(int tip){
-        this.tip = tip;
-    }
+  }
+   
+public twoSum(){
+    
+}   
+  // java learn 
+   
+   
+// oops learning   
+    // public static void main(String[] args) {
+    // Pen p1 =new Pen();
+    // p1.setColor("orange");
+    //     System.out.println(p1.getColor());
+    //     p1.setTip(4);
+    //     System.out.println(p1.getTip());
+    // Pen p2 = new Pen();
+    // p2.setColor("green");
+    //     System.out.println(p2.getColor());
+    //     p2.setTip(6);
+    //     System.out.println(p2.getTip());
+    // }
 }
+
+
+
+
+//  oops learning
+// class Pen{
+//     private String color;
+//     private int tip;
+
+//     String getColor(){
+//     return this.color;
+//     }
+//     int getTip(){
+//         return this.tip;
+//     }
+//     void setColor(String newColor){
+//     this.color = newColor;
+//     }
+//     void setTip(int tip){
+//         this.tip = tip;
+//     }
+// }
